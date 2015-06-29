5832f9cbd7010d23c18e719febec995422436976
pote-mayo <goto20110921@gmail.com>
2015-06-29 16:17:40 +0900

    ・デバッグ(this->log)をくっつけてみた。 ・うまくログインできない。パスワードハッシャーのせいかもしれない。

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -7,13 +7,15 @@ use Cake\ORM\TableRegistry;
 class UsersController extends AppController {
 	public function login()
 	{
+// log/debug.logに出力されます。
+$this->log('login', 'debug');
 		if ($this->request->is('post')) {
 			$user = $this->Auth->identify();
 			if ($user) {
 				$this->Auth->setUser($user);
 				return $this->redirect($this->Auth->redirectUrl());
 			}
-			$this->Flash->error('Your username or password is incorrect.');
+			$this->Flash->error('Your email or password is incorrect.');
 		}
 	}
 }
